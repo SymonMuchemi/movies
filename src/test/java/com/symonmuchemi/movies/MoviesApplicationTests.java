d983d194dad1b6a165a5bc59424d986f2193aa7b
symonmuchemi <simonkenya846@gmail.com>
2024-02-24 03:33:10 +0300

    Hello world

---
+++ src/test/java/com/symonmuchemi/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.symonmuchemi.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
