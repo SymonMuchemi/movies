96582003ac88dc1e429f7abb18c6ee7cf712f724
symonmuchemi <simonkenya846@gmail.com>
2024-02-26 03:57:21 +0300

    feat: added a createReview method This method uses a MongoTemplate class to match the movie to be updated using the imdbId and applies the update as well

---
+++ src/main/java/com/symonmuchemi/movies/ReviewService.java
@@ -0,0 +1,29 @@
+package com.symonmuchemi.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    public Review createReview (String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+
+         /* updating the Movie collection by adding a
+           new reviewsId to the Movie identified with imbdId
+          */
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))// Specifies which movie to update
+                .apply(new Update().push("reviewsId").value(review));
+
+        return review;
+    }
+}
